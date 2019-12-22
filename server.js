cf47db6577d03f0720c96e8d7975f57b46224212
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-12-22 14:15:53 -0500

    commiting just in case before I deploy to heroku

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get("/newscrape", function (req, res) {
             link = link + $(element).parents("a").attr("href");
             var summaryOne = $(element).parent().parent().siblings().children("li:first-child").text();
             var summaryTwo = $(element).parent().parent().siblings().children("li:last-child").text();
-
+    
             if (headline && summaryOne && link) {
                 results.push({
                     headline: headline,
